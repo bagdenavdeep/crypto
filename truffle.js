d6def368725e31881207f0c6f1d0d187bb57e84a
Vladimir Umantsev <v.umancev@404-group.com>
2017-10-13 14:19:13 +0300

    Remove gas limit for rinkeby

--- truffle.js
+++ truffle.js
@@ -10,8 +10,8 @@ module.exports = {
 			host: "localhost", // Connect to geth on the specified
 			port: 8545,
 			from: "0xC82d0554D5ee7AC2e2eb1FBE0400De426DD2CFC5", // default address to use for any transaction Truffle makes during migrations
-			network_id: 4,
-			gas: 4712388 // Gas limit used for deploys
+			network_id: 4
+			// gas: 6125124 // Gas limit used for deploys
 		}
 	}
 };
